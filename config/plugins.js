72ffd6c822ec496053c9abd3ef8951eeed19cb1a
Hieu Tran <tranhieunz@gmail.com>
2022-06-07 00:25:18 +1200

    Update: Adding the send email provider in backend

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,18 @@
-module.exports = {
+module.exports = ({ env }) => ({
   wysiwyg: {
     enabled: true,
     resolve: "./src/plugins/wysiwyg", // path to plugin folder
   },
-};
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'nz.vincent.tran@gmail.com',
+        defaultReplyTo: 'nz.vincent.tran@gmail.com',
+      },
+    },
+  },
+});
